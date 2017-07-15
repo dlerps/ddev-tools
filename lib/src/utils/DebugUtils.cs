ae4add824a3531fd84264201971cb29406dd7d77
Daniel Lerps <d@niel.lerps.de>
2017-07-15 22:40:53 +0200

    Debug Utils

---
+++ lib/src/utils/DebugUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDev.Tools
+{
+    public static class DebugUtils
+    {
+        /// <summary>
+        /// Convenience method which combines String.Format and Console.WriteLine(...)
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="msgParams"></param>
+        public static void Print(string msg, params object[] msgParams)
+        {
+            Console.WriteLine(String.Format(msg, msgParams));
+        }
+    }
+}
